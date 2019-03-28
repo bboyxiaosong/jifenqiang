6d634819d5d8d8c541828af039300d3cbb345bb7
liqian <liqian@baihe.com>
2019-03-28 19:15:06 +0800

    hanxs

---
+++ js/cashList.js
@@ -0,0 +1,71 @@
+/*
+ 
+ * by xiaosong 2019.3.6
+ * 
+ * */
+
+var isbool = true;
+$(function(){
+	addLoadingCtrl();
+	var userObj = get('userObj');//过期时间为1周
+    if (userObj) {
+    		userObj.pageNo = 1;
+    		userObj.pageSize = 20;
+    		billListCtrl(userObj);
+    }else{
+       window.location.href="index.html";
+       window.close();
+    }
+    
+     window.onscroll = function() {
+        if (getScrollTop() + getWindowHeight() >= getScrollHeight() - 20 && isbool == true) {
+            isbool = false;
+            userObj.pageNo += 1;
+            setTimeout(function(){
+                billListCtrl(params);
+            },300)
+        } 
+    }
+});
+//4.6	获取会员账务明细
+function billListCtrl(params){
+	getJsonpHtml('/baihe-adserver/user/bill/list',params,function(data){
+ 		if(data.code == 0){
+		 	taskListCtrl(data.data.accountdetailslist,params);
+		}else{
+			errorAlert(data.msg);
+		}
+	 	},function(e){
+	 		
+ 	});
+ 	isbool = true;
+}
+
+function taskListCtrl(arr,params){
+	var ul_wrap = $('.cash-ul');
+	ul_wrap.html('');
+	var str = '';
+	if(arr.length > 0){
+		$.each(arr, function(index,ele) {
+			ele.statusName = ele.status== 0?'已成功':'待审核';
+			
+			ele.amount = ele.amount/100;
+			str +='<div class="cash-list">'
+				+'<div class="left-wrap">'
+				+'	<div class="store-name">'+ele.memo+'</div>'
+				+'	<div class="cash-times">'+getLocalTime(ele.createtime,3)+ '<span class="tixian_time">'+getLocalTime(ele.createtime,6)+'</span></div>'
+				+'</div>'
+				+'<div class="center-wrap">'+ele.statusName+'</div>'
+				+'<div class="right-wrap">'+ele.amount+'</div>'
+				+'<div class="clear"></div>'
+				+'</div>';
+		});
+		if(params.pageNo == 1){
+			ul_wrap.html(str);
+		}else{
+			ul_wrap.append(str);
+		}
+	}else{
+		ul_wrap.html('<div class="no_content">暂无账务明细</div>');
+	}
+}
---
+++ js/taskHistory.js
@@ -0,0 +1,98 @@
+
+/*
+ * 
+ by xiaosong  2019.3
+ * 历史任务列表
+ * */
+var isbool = true;
+$(function(){
+	addLoadingCtrl();
+	var userObj = get('userObj');//过期时间为1周
+    if (userObj) {
+    		userObj.pageNo = 1;
+    		userObj.pageSize = 20;
+    	 	taskHitstoryCtrl(userObj);
+    }else{
+       window.close();
+       window.location.href="index.html";
+    }
+    window.onscroll = function() {
+        if (getScrollTop() + getWindowHeight() >= getScrollHeight() - 20&&isbool==true) {
+            isbool = false;
+            userObj.pageNo += 1;
+            setTimeout(function(){
+                taskHitstoryCtrl(params);
+            },300)
+        } 
+    }
+    
+});
+//4.8	会员历史任务
+function taskHitstoryCtrl(params){
+	getJsonpHtml('/baihe-adserver/user/history/tasks',params,function(data){
+		if(data.code == 0){
+			 taskListCtrl(data.data.tasklist,params);
+		}else{
+			errorAlert(data.msg);
+		}
+	},function(e){
+		
+	});
+	 isbool = true;
+	
+}
+//4.11	用户确认完成任务
+function completeCtrl(params){
+	getJsonpHtml('/baihe-adserver/user/task/complete',params,function(data){
+		if(data.code == 0){
+			var params = {};
+			params.uid = params.uid;
+			taskHitstoryCtrl(params);
+		}else{
+			errorAlert(data.msg);
+		}
+	},function(e){
+		
+	});
+}
+function sureCtrlFn(ele){
+	var self = $(ele);
+	var tid = self.attr('data-tid');
+	var userObj = get('userObj');//过期时间为1周
+	userObj.tid = tid;
+	if(self.hasClass('status_0')){
+		completeCtrl(userObj);
+	}
+}
+
+function taskListCtrl(arr,params){
+	var ul_wrap = $('.task-ul');
+	ul_wrap.html('');
+	var str = '';
+	var statusStr = '';
+	if(arr.length > 0){
+		$.each(arr, function(index,ele) {
+				console.log(ele);
+				ele.statusName = ele.status== 0?'未完成</br>请确认':ele.status== 1?'待审核':ele.status==2?'已完成':ele.status==3?'已失效':'未通过';
+				
+				str+='<div class="task-li">'
+					+'<div class="lf-img" style="background: url('+ele.pic+') no-repeat center center;"></div>'
+					+'<div class="left-wrap">'
+					+'	<div class="store-name">'+ele.tname+' <span style="color: #f8b526;">'+(ele.award/100)+'元</span></div>'
+					+'	<div class="cash-times">'+getLocalTime(ele.expiretime,3)+'<span class="daoqi-time">'+getLocalTime(ele.expiretime,6)+' 到期</span></div>'
+					+'</div>'
+					+'<div class="right-wrap status_'+ele.status+'" data-tid="'+ele.tid+'" onclick="sureCtrlFn(this)" >'+ele.statusName+'</div>'
+					+'<div class="clear"></div>'
+				+'</div>';
+		});
+		
+		if(params.pageNo == 1){
+			ul_wrap.html(str);
+		}else{
+			ul_wrap.append(str);
+		}
+	}else{
+		ul_wrap.html('<div class="no_content">暂无历史任务 ！</div>');
+	}
+	
+}
